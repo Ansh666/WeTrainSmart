9fde2b179848b7a0c0a4a2bbc7a46bebdfc751bd
wetrainsmart <wetrainsmart@gmail.com>
2023-08-14 23:34:36 +0530

    newCC

--- WeTrainSmart/settings.py
+++ WeTrainSmart/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'WeTrainSmart.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'mbdb/db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR / 'mbdb/db.sqlite3'),
     }
 }
 
