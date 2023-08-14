4a58dbb79d36a00c77dd4c880f11a88a8d13e506
wetrainsmart <wetrainsmart@gmail.com>
2023-08-15 04:17:55 +0530

    newCom

--- WeTrainSmart/settings.py
+++ WeTrainSmart/settings.py
@@ -137,8 +137,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 AUTH_USER_MODEL = "user.CustomUser"
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ORIGIN_ALLOW_ALL = True
+
 CORS_ALLOWED_ORIGINS = [
     "http://www.wetrainsmart.co.in",
     "http://wetrainsmart.co.in",
