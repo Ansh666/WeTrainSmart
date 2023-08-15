608eb3811d94fd7ccdc2bdfffdc2c2c76aba7f36
wetrainsmart <wetrainsmart@gmail.com>
2023-08-16 00:58:15 +0530

    wsgiupdate

--- WeTrainSmart/wsgi.py
+++ WeTrainSmart/wsgi.py
@@ -6,9 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
-
+import sys
 import os
-
+sys.path.append('/var/www/WeTrainSmart')
+sys.path.append('/var/www/WeTrainSmart/env/Lib//site-packages')
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WeTrainSmart.settings')
