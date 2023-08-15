9b42f14f538abb762c783aaf531cf2c02c5a02d7
wetrainsmart <wetrainsmart@gmail.com>
2023-08-16 00:21:02 +0530

    Set1

--- WeTrainSmart/wsgi.py
+++ WeTrainSmart/wsgi.py
@@ -8,12 +8,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
-sys.path.append('/var/www/WeTrainSmart')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WeTrainSmart.settings')
 from django.core.wsgi import get_wsgi_application
-from django.contrib.auth.handlers.modwsgi import check_password, groups_for_user
-from django.core.handlers.wsgi import WSGIHandler
 
-application = WSGIHandler()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WeTrainSmart.settings')
+
+application = get_wsgi_application()
