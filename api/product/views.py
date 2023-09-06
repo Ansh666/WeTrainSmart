bace224839c3505aa563aa5a69e4d3846e0427fd
wetrainsmart <wetrainsmart@gmail.com>
2023-09-06 15:39:07 +0530

    seet

--- api/product/views.py
+++ api/product/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 from .serializers import ProductSerializer
 from .models import Product
 
@@ -7,5 +8,14 @@ from .models import Product
 
 
 class ProductViewSet(viewsets.ModelViewSet):
+    permission_classes_by_action = {'create': [AllowAny]}
     queryset = Product.objects.all().order_by('id')
     serializer_class = ProductSerializer
+   
+    
+    def get_permissions(self):
+        try:
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+
+        except KeyError:
+            return [permission() for permission in self.permission_classes]
