3b7cdc7f66da67d6834143d5e6398c18428379b4
wetrainsmart <wetrainsmart@gmail.com>
2023-09-06 18:11:29 +0530

    tr

--- WeTrainSmart/settings.py
+++ WeTrainSmart/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
-
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'WeTrainSmart.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'mbdb/db.sqlite3'),
+        'NAME': BASE_DIR / 'mbdb/db.sqlite3',
     }
 }
 
@@ -133,9 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 AUTH_USER_MODEL = "user.CustomUser"
