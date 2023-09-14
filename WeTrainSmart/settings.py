9027776c19e03baaf7d96d0fd8489e7417d58ac0
wetrainsmart <wetrainsmart@gmail.com>
2023-09-15 01:19:19 +0530

    pillow

--- WeTrainSmart/settings.py
+++ WeTrainSmart/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'PIL',
     'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
