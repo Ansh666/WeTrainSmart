7df0bda3dc832b971adb3c9b9e4d54c572486828
wetrainsmart <wetrainsmart@gmail.com>
2023-09-14 19:25:59 +0530

    offer

--- api/offer/admin.py
+++ api/offer/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import offer
+from .models import Offer
 # Register your models here.
-admin.site.register(offer)
+admin.site.register(Offer)
